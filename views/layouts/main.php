6c2550b8a0cedc68728d654b81caf702d3c68020
Birnchen <birnchen@birnchen-interactive.de>
2019-02-25 17:29:38 +0100

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -66,7 +66,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Birnchen Interactive 2016 - <?= date('Y') ?></p>
     </div>
 </footer>
 
