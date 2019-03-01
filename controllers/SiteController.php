3f53dfcd53685bab2db590ea08c66f7c891e51dc
Birnchen <birnchen@birnchen-interactive.de>
2019-03-01 10:33:31 +0100

    Update SiteController.php

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -85,7 +85,8 @@ class SiteController extends Controller
     public function actionLogin()
     {
         if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
+            return $this->render('index');
+            //return $this->goHome();
         }
 
         $model = new LoginForm();
