8f9f2f7fa7fbaa5e2d2b63de1e627302163fa70d
Birnchen <birnchen@birnchen-interactive.de>
2019-03-01 17:08:49 +0100

    Create twitch.php

---
+++ views/site/twitch.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Twitch.tv';
+?>
+<div class="site-twitch-tv">
+    <iframe
+        src="https://player.twitch.tv/?channel=stage5_tv"
+        height="720"
+        width="1280"
+        frameborder="0"
+        scrolling="no"
+        allowfullscreen="true">
+    </iframe>
+</div>
