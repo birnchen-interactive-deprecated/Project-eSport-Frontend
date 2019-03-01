353d834e71dfc7ea1e8b34bc26800803ec8c92d7
CaptFreezer <freezerxxl@gmx.net>
2019-03-01 16:48:51 +0100

    Footer schoener gemacht

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -11,6 +11,15 @@ use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 
 AppAsset::register($this);
+
+$visible = (Yii::$app->user->isGuest) ? false : true;
+
+$twitterImg = Html::img('images/Twitter_Logo_Blue.png', ['height' => '50px']);
+$twitterLink = Html::a($twitterImg, 'https://twitter.com/esport_project', ['target' => '_blank']);
+
+$discordImg = Html::img('images/Discord-Logo-White.png', ['height' => '50px', 'style' => 'padding: 5px 0; ']);
+$discordLink = Html::a($discordImg, 'https://discord.gg/f6NXNFy', ['target' => '_blank']);
+
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -38,8 +47,8 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-                ['label' => 'Welcome', 'url' => ['/site/index']],
-                ['label' => 'Cups', 'items' => [
+                ['label' => 'Welcome', 'visible' => $visible, 'url' => ['/site/index']],
+                ['label' => 'Cups', 'visible' => $visible, 'items' => [
                     ['label' => '1v1 Cup', 'url' => ['/site/news']],
                     ['label' => '2v2 Cup', 'url' => ['/site/account']],
                     ['label' => '3v3 Cup', 'url' => ['/site/tournament']],
@@ -72,7 +81,14 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="make-center">&copy; Birnchen Interactive 2016 - <?= date('Y') ?></p>
+        <div class="col-lg-2">
+            
+        </div>
+        <div class="col-lg-6 col-lg-offset-1 make-center" style="padding: 15px 0 0;">&copy; Birnchen Interactive 2016 - <?= date('Y') ?></div>
+        <div class="col-lg-2 col-lg-offset-1">
+            <span><?= $twitterLink; ?></span>
+            <span><?= $discordLink; ?></span>
+        </div>
     </div>
 </footer>
 
