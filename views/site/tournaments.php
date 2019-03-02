4d70df53ae86c1664c74a815be1bc4f97cf2b786
Birnchen <birnchen@birnchen-interactive.de>
2019-03-02 15:23:51 +0100

    Update tournaments.php

--- views/site/tournaments.php
+++ views/site/tournaments.php
@@ -6,14 +6,15 @@ use yii\helpers\Html;
 
 $teams = array(
     'Team Name 1' => array(
+        'logo' => 'images/teams/Captain_Viper.png',
         'main' => array(
-            'Spieler 1',
-            'Spieler 2',
-            'Spieler 3',
+            'Captain Salty',
+            'Mr. Viper',
+            'El Viper',
         ),
         'subs' => array(
-            'Ersatzspieler 1',
-            'Ersatzspieler 2',
+            'Corah',
+            'Marc',
         ),
     ),
     'Team Name 2' => array(
@@ -34,21 +35,32 @@ $this->title = 'Tournaments';
 <div class="site-tournaments">
 
     <ul>
-    <?php foreach($teams as $teamname => $tmpArr): ?>
-        <li class="teamName"><?php echo $teamname ?>
-            <ul>
-        <?php foreach($tmpArr as $mainSub => $memberArr): ?>
-            <li><?= ($mainSub === 'main') ? 'Mainspieler' : 'Ersatzspieler'; ?>
+        <?php foreach($teams as $teamname => $tmpArr): ?>
+            <?php if (isset($tmpArr['logo'])): ?>
+                <?= Html::img($tmpArr['logo'], ['width' => '150px', 'height' => '150px']); ?>
+            <?php endif; ?>
+
+            <li class="teamName"><?php echo $teamname ?>
                 <ul>
-                    <?php foreach($memberArr as $member): ?>
-                    <li class="memberName"><?php echo $member ?></li>
+                    <?php foreach($tmpArr as $mainSub => $memberArr): ?>
+
+                        <?php if ($mainSub === 'main' || $mainSub === 'subs'): ?>
+
+                            <li><?= ($mainSub === 'main') ? 'Mainspieler' : 'Ersatzspieler'; ?>
+                                <ul>
+                                    <?php foreach($memberArr as $member): ?>
+                                        <li class="memberName"><?php echo $member ?></li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            </li>
+                        <?php endif; ?>
+
                     <?php endforeach; ?>
+
                 </ul>
             </li>
+
         <?php endforeach; ?>
-            </ul>
-        </li>
-    <?php endforeach; ?>
     </ul>
 
 </div>
