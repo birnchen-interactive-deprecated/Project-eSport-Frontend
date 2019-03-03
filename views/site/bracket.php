c0712e2888ff5fc2b10afe70d6a1dc48f744f322
Birnchen <birnchen@birnchen-interactive.de>
2019-03-03 14:06:12 +0100

    Update bracket.php

--- views/site/bracket.php
+++ views/site/bracket.php
@@ -9,3 +9,8 @@ $this->title = 'Bracket';
 <div class="site-bracket">
     <iframe src="https://challonge.com/de/450airl6/module" width="100%" height="720" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
 </div>
+
+<div class="nothing">
+    <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=DE&source=ac&ref=tf_til&ad_type=product_link&tracking_id=birnchenstudi-21&marketplace=amazon&region=DE&placement=B077XWNR96&asins=B077XWNR96&linkId=54d5de4dea20b5ba63c289ad048233da&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff">
+    </iframe>
+</div>
