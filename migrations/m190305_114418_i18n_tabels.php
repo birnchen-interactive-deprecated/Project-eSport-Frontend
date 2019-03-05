b72bdbc0b1f5408132193795c25af97e9a9fafbf
Birnchen <47638797+Birnchen@users.noreply.github.com>
2019-03-05 12:44:31 +0100

    Create m190305_114418_i18n_tabels.php

---
+++ migrations/m190305_114418_i18n_tabels.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190305_114418_i18n_tabels
+ */
+class m190305_114418_i18n_tabels extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190305_114418_i18n_tabels cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190305_114418_i18n_tabels cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
