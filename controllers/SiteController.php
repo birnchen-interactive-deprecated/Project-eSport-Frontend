608468915549f01bb38f7b7950c499dc2d3821e6
Birnchen <birnchen@birnchen-interactive.de>
2019-03-11 17:17:56 +0100

    Update SiteController.php

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -150,7 +150,7 @@ class SiteController extends Controller
         }
 
         $OwnedMainTeam = [];
-        foreach ( Main_Team::find()->where('ownerID' == $userId) as $mainTeam) {
+        foreach ( Main_Team::find()->all() as $mainTeam) {
             if($mainTeam->getOwnerId() == $userId)
             {
                 $OwnedMainTeam['teamID'] = $mainTeam->getId();//
