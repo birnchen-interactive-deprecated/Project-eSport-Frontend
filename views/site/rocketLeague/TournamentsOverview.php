67aecc89ffdd81245324476f269fde7ca7738a54
Birnchen <birnchen@birnchen-interactive.de>
2019-03-12 22:13:09 +0100

    jpg  to png

--- views/site/rocketLeague/TournamentsOverview.php
+++ views/site/rocketLeague/TournamentsOverview.php
@@ -8,7 +8,6 @@ $this->title = '1v1 Tournaments';
 ?>
 <div class="site-news">
 
-    1v1 Tournaments
 	<?php foreach ($tournamentList as $key => $tournament): ?>
 		<div><?= $tournament->getTournamentName(); ?></div>
 	<?php endforeach; ?>
