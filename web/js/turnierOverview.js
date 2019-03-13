aef41dc44fdd6d0ada1454f9b6e63129f3b8f5b1
Philipp Jarnig <philipp.jarnig@gwp.dpd.at>
2019-03-13 15:16:16 +0100

    Bugfixing...

--- web/js/turnierOverview.js
+++ web/js/turnierOverview.js
@@ -1,8 +1,9 @@
 "use strict";
 
 $(document).ready(function() {
-	$('.site-rl-tournament-overview .turnierStatus.foldable .namedHeader').click(function() {
+	$('.site-rl-tournament-overview .turnierStatus.foldable .namedHeader').on('click', function() {
 		var tableParent = $(this).parents('table.foldable');
+		console.log(tableParent);
 		if (tableParent.hasClass('foldable_open')) {
 			tableParent.removeClass('foldable_open');
 		} else {
