d5c3789fef26f3e8bd888627dd47c6a4073400c0
Philipp Jarnig <philipp.jarnig@gwp.dpd.at>
2019-03-13 15:19:14 +0100

    Console.log entfernt

--- web/js/turnierOverview.js
+++ web/js/turnierOverview.js
@@ -1,13 +1,16 @@
 "use strict";
 
 $(document).ready(function() {
+
 	$('.site-rl-tournament-overview .turnierStatus.foldable .namedHeader').on('click', function() {
+
 		var tableParent = $(this).parents('table.foldable');
-		console.log(tableParent);
 		if (tableParent.hasClass('foldable_open')) {
 			tableParent.removeClass('foldable_open');
 		} else {
 			tableParent.addClass('foldable_open');
 		}
+
 	})
+	
 });
