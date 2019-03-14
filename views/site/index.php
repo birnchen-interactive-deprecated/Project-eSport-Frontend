326538302faad7a38ff8d6994476196512c88131
Birnchen <birnchen@birnchen-interactive.de>
2019-03-14 23:17:54 +0100

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -8,6 +8,7 @@ $this->title = 'Welcome';
 ?>
 <div class="site-index">
     <div class="welcome">
-        Willkommen auf unserer Turnier Website.
+        Willkommen auf unserer Turnier Website.<br>
+        Besuch uns doch bei fragen auf unserem <a class="dclink" href="https://discord.gg/f6NXNFy">Discord</a> Server.
     </div>
 </div>
