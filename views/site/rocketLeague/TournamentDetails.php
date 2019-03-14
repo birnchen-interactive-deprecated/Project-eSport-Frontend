196ada18e6263a416f8d15c9a501223a35d6fee2
Birnchen <birnchen@birnchen-interactive.de>
2019-03-14 17:16:22 +0100

    Update TournamentDetails.php

--- views/site/rocketLeague/TournamentDetails.php
+++ views/site/rocketLeague/TournamentDetails.php
@@ -15,8 +15,8 @@ $this->title = 'Turnier Details';
 
     <div class="rulesHeader"><?= $ruleSet['baseSet']; ?></div>
 
-    <?php foreach ($ruleSet as $key => $subRuleSet): ?>
-        
+    <?php foreach ($ruleSet['subRulesSet'] as $key => $subRuleSet): ?>
+        <?php $paragraph = $subRuleSet->GetRulesParagraph(); ?>
         <div class="rulesBody"><?= " ".$subRuleSet->getSubRuleName(); ?></div>
         <div class="rulesBody"><?= $subRuleSet->getSubRuleDescription(); ?></div>
     <?php endforeach; ?>
