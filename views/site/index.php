33fee9a880e3f8a310ac78e73f2bd25c87f9a0b2
Birnchen <birnchen@birnchen-interactive.de>
2019-03-14 23:22:18 +0100

    index.php

--- views/site/index.php
+++ views/site/index.php
@@ -9,6 +9,9 @@ $this->title = 'Welcome';
 <div class="site-index">
     <div class="welcome">
         Willkommen auf unserer Turnier Website.<br>
-        Besuch uns doch bei fragen auf unserem <a class="dclink" href="https://discord.gg/f6NXNFy">Discord</a> Server.
+        Besuch uns doch bei Fragen auf unserem <a class="dclink" href="https://discord.gg/f6NXNFy">Discord</a> Server.
+        <br>
+        Noch kein Team angelegt??? <br>
+        Melde dich auf <a class="dclink" href="https://discord.gg/f6NXNFy">Discord</a> bei <b><i><u>Birnchen | Pierre#5366</u></i></b>.
     </div>
 </div>
