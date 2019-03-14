7d0ac34506ef2dbbc86a0514ee37b00cfd91597e
Birnchen <birnchen@birnchen-interactive.de>
2019-03-14 21:11:38 +0100

    redirecting

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -70,7 +70,6 @@ class SiteController extends Controller
     public function actionIndex()
     {
         if (Yii::$app->user->isGuest) {
-            // return $this->render('index');
             return $this->goHome();
         }
 
--- views/site/index.php
+++ views/site/index.php
@@ -9,19 +9,6 @@ $this->title = 'Welcome';
 <div class="site-index">
     <?php if (!Yii::$app->user->isGuest): ?>
         <div class="welcome">
-            Willkommen zum ersten Spieltag der zweiten Season des Gerta Cups. <br>
-            Aufgrund technischer Probleme wird der Checkin über unseren <br>
-            <a class="dclink" href="https://discord.gg/f6NXNFy">Discord</a> <br>
-            ablaufen. Alle Registrierten User mögen dort Bitte um 16:45 eintreffen <br>
-            und im Warteraum warten. <br>
-            Checkin ist von 17:00 - 17:15, wer nicht da ist in dieser Zeit wird nicht eingecheckt. <br>
+            Willkommen auf unserer Turnier Website.
         </div>
-        <iframe class="regeln"
-                src="https://docs.google.com/document/d/e/2PACX-1vR66PMmQPCHbttNuV5IuRwPj0wPzrxe03-xBIyu1r-gWfIuBKnZyQ2ELYYEGKZQ4OFaunfwJWQtNOW9/pub?embedded=true"></iframe>
-    <?php else: ?>
-        <div class="LoggingIn">
-            Please <?= Html::a("Login", ['login']); ?> to see all Informations <br>
-            Bitte <?= Html::a("Logge", ['login']); ?> dich ein um alle Informationen zu sehen.
-        </div>
-    <?php endif; ?>
 </div>
