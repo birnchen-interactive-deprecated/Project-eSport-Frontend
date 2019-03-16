2738efbee20e054bfa93e0fbb43185dc4bf8d538
CaptFreezer <freezerxxl@gmx.net>
2019-03-16 12:59:33 +0100

    Turnierbaum auch nur bis Tagesende anzeigen

--- views/site/rocketLeague/TournamentDetails.php
+++ views/site/rocketLeague/TournamentDetails.php
@@ -64,6 +64,8 @@ switch ($tournament->getModeId()) {
 }
 
 $turnierStart = new DateTime($tournament->getDtStartingTime());
+$turnierEnd = new DateTime($tournament->getDtStartingTime());
+$turnierEnd->setTime(23, 59, 59);
 
 $this->title = 'Turnier Details';
 ?>
@@ -198,7 +200,7 @@ $this->title = 'Turnier Details';
         </tbody>
     </table>
 
-    <?php if ($now->diff($turnierStart)->invert == 1): ?>
+    <?php if ($now->diff($turnierStart)->invert == 1 && $now->diff($turnierEnd)->invert == 0): ?>
         <iframe src="https://challonge.com/de/<?= $challongeId; ?>/module" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
     <?php else: ?>
         <b>!!!</b> Hier erscheint nach Turnierstart der Turnierbaum <b>!!!</b>
