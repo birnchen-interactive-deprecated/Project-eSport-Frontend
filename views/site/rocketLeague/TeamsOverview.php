e4bc69e5b0d3f054f18c0de326e34b8cd16f34a4
Birnchen <birnchen@birnchen-interactive.de>
2019-03-16 12:37:31 +0100

    Update TeamsOverview.php

--- views/site/rocketLeague/TeamsOverview.php
+++ views/site/rocketLeague/TeamsOverview.php
@@ -10,7 +10,7 @@ $this->title = 'Turnier Details';
 ?>
 
 <div class="site-rl-tournament-details">
-    <?php foreach($subteams as $key => $subteam ) : ?>
+    <?php foreach($subteams as $subteam ) : ?>
     <?= Html::a($subteam->getName() , ['/site/team-details', 'id' => $subteam->getId()]) ?>
     <?php endforeach; ?>
 </div>
