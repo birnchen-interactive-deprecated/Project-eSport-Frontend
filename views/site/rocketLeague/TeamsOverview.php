78184254bc6f00d13780e916eff3544e934b2a4e
Birnchen <birnchen@birnchen-interactive.de>
2019-03-16 14:17:08 +0100

    Update TeamsOverview.php

--- views/site/rocketLeague/TeamsOverview.php
+++ views/site/rocketLeague/TeamsOverview.php
@@ -15,7 +15,7 @@ $this->title = 'Turnier Details';
     foreach($teamHierarchy as $hierarchy )  {
 
 		$mainTeam = $hierarchy['mainTeam'];
-		$mainTeamOwner = $mainTeam->getOwner()->one()->getUserName();
+		$mainTeamOwner = $mainTeam->getOwner()->one()->getUsername();
         echo Html::a($mainTeam->getName() , ['/site/team-details', 'id' => $mainTeam->getId()]) . '(' . Html::a($mainTeamOwner , ['/site/team-details', 'id' => $mainTeam->getOwnerId()]) . ')' .  '<br>';
 
 
@@ -23,8 +23,9 @@ $this->title = 'Turnier Details';
 
 			$subTeam = $subHierarchy['subTeam'];
 			$subTeamName = $subTeam->getName() . ' ' . $subTeam->getTournamentMode()->one()->getName();
+			$subTeamManager = $subTeam->GetTeamCaptain()->one()->getUsername();
 
-			echo Html::a($subTeamName , ['/site/team-details', 'id' => $subTeam->getId()]) . '<br>';
+			echo Html::a($subTeamName , ['/site/team-details', 'id' => $subTeam->getId()]) . '(' . Html::a($subTeamManager , ['/site/team-details', 'id' => $subTeam->getTeamCaptainId()]) . ')' .'<br>';
 
 			foreach ($subHierarchy['subTeamMember'] as $key => $subTeamMember) {
 
