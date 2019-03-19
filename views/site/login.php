24fbf39edf6fac075839493451e8f7a5f6ad6b33
Birnchen <birnchen@birnchen-interactive.de>
2019-03-20 00:25:51 +0100

    Update login.php

--- views/site/login.php
+++ views/site/login.php
@@ -9,6 +9,28 @@ use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
 
 $this->title = 'Login';
+
+$this->registerMetaTag([
+    'name' => 'og:title',
+    'content' => $model->username.'Login',
+]);
+$this->registerMetaTag([
+    'name' => 'og:description',
+    'content' => 'you must be logged in to see something',
+]);
+$this->registerMetaTag([
+    'name' => 'og:url',
+    'content' => 'https://project-esport.gg/site/login',
+]);
+$this->registerMetaTag([
+    'name' => 'og:type',
+    'content' => 'website',
+]);
+$this->registerMetaTag([
+    'name' => 'og:image',
+    'content' => 'https://birnchen-studios.de/wp-content/uploads/birnchen_logo_.png',
+]);
+
 ?>
 <div class="site-login">
     <h1 class="col-lg-offset-1"><?= Html::encode($this->title) ?></h1>
