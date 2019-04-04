29fbe2bbf4601a0c49d3571d7c2bd65b9fcc439e
Birnchen <47638797+Birnchen@users.noreply.github.com>
2019-04-04 12:23:15 +0200

    backend refactoring

--- views/site/index.php
+++ views/site/index.php
@@ -1,39 +1,12 @@
 <?php
 
-/* @var $this yii\web\View */
+/* @var $this yii\web\View
+ * @var $data array
+ */
 
 use yii\helpers\Html;
 
 $this->title = 'Welcome to Project eSport\'s';
-
-$data = Yii::$app->cache->get('RSS_FEED_RL');
-if (false === $data) {
-
-	$curl = curl_init('https://steamcommunity.com/games/252950/rss/');
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	$rssFeed = curl_exec($curl);
-
-	$xml = simplexml_load_string($rssFeed);
-
-	$data = [];
-
-	$key = 0;
-	foreach ($xml->channel->item as $item) {
-
-		if (3 === $key) {
-			break;
-		}
-
-		$data[$key++] = [
-			'title' => $item->title->__toString(),
-			'html' => $item->description->__toString(),
-		];
-
-	}
-
-	$cacheDuration = 300; // Einheit = Sekunden
-	Yii::$app->cache->set('RSS_FEED_RL', $data, $cacheDuration);
-}
 
 
 $this->registerLinkTag(['rel' => 'canonical', 'href' => 'https://project-esport.gg' . Yii::$app->request->url]);
