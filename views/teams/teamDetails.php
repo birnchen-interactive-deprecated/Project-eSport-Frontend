cc97c63f985e7edac71b136d320aea90f1f4344c
Philipp Jarnig <philipp.jarnig@gwp.dpd.at>
2019-04-15 13:00:20 +0200

    bugfix

--- views/teams/teamDetails.php
+++ views/teams/teamDetails.php
@@ -80,7 +80,7 @@ Yii::$app->metaClass->writeMetaMainTeam($this, $teamDetails, $this->title);
             <div class="col-xs-5 col-sm-3 col-lg-3">Team-Members</div>
             <div class="col-xs-7 col-sm9 col-lg-9 context">
                 <?php foreach($teamDetails->getTeamMember()->all() as $userKey => $user): ?>
-                    <div class="col-lg-6"><?= $user->getUsername(); ?></div>
+                    <div class="col-lg-6"><?= $user->getUser()->one()->getUsername(); ?></div>
                 <?php endforeach; ?>
             </div>
         </div>
