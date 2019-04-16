dd541eff6e5eba7259d73c102dd0cf6e37e52760
Philipp Jarnig <philipp.jarnig@gwp.dpd.at>
2019-04-16 13:38:03 +0200

    SubTeams wirklich sortieren.

--- modules/core/models/MainTeam.php
+++ modules/core/models/MainTeam.php
@@ -156,7 +156,7 @@ class MainTeam extends ActiveRecord
 
         if ($sort) {
             foreach ($subTeamsGrouped as $subTeamKey => $subTeams) {
-                usort($subTeams, function($a, $b) {
+                usort($subTeamsGrouped[$subTeamKey], function($a, $b) {
                     return $a->getName() > $b->getName();
                 });
             }
